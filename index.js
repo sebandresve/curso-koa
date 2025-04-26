5ad1ed71d38fd8e6f3309f73c83eaa67f2625aa4
sebandresve <sebandres.ve@gmail.com>
2025-04-26 11:08:23 -0400

    se agrega koa-router

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 import Koa from 'koa'
 import { setFinalResponseMdw, setResponseTimeMdw } from './middlewares.js'
+import Router from 'koa-router'
 
 const app = new Koa()
+const router = new Router()
 
-// piso 1 (primer middleware)
 app.use(setFinalResponseMdw)
-
-// piso 2 (segundo middleware)
 app.use(setResponseTimeMdw)
 
-// piso 3 (tercer middleware)
-app.use(async ctx => {
-  ctx.body = 'Hello World'
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+
+router.get('/user', (ctx, next) => {
+  ctx.body = { ok: true, message: 'Hola GET' }
+})
+
+router.post('/user', (ctx, next) => {
+  ctx.body = { ok: true, message: 'Hola POST' }
+})
+
+router.put('/user', (ctx, next) => {
+  ctx.body = { ok: true, message: 'Hola PUT' }
+})
+
+router.delete('/user', (ctx, next) => {
+  ctx.body = { ok: true, message: 'Hola DELETE' }
 })
 
 app.listen(3000, () => console.log('Server running on http://localhost:3000'))
