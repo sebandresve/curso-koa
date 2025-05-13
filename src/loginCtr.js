e9f91c3d5b9885d6f52860b46d9d9f48db569053
sebandresve <sebandres.ve@gmail.com>
2025-05-13 15:39:46 -0400

    se agrega path login con creacion de jwt

--- src/loginCtr.js
+++ src/loginCtr.js
@@ -1,4 +1,6 @@
 import { UserRepository } from '../database/UserRepository.js'
+import { comparePassword } from './utils/hashPassword.js'
+import { createToken } from './utils/tokenGenerator.js'
 
 const signIn = async (ctx) => {
   const { email, password } = ctx.request.body
@@ -10,9 +12,24 @@ const signIn = async (ctx) => {
 
   const foundUser = await UserRepository.getUserByEmail(email) // funcion que obtiene el usuario por email
 
+  const passwordMatch = await comparePassword(password, foundUser.password) // funcion que compara la contraseña
+
+  if (!passwordMatch) {
+    ctx.status = 401
+    return
+  }
+
+  const jwt = createToken({ id: foundUser.id, email: foundUser.email }) // funcion que crea el token
+
+  const data = {
+    id: foundUser.id,
+    email: foundUser.email
+  }
+
   ctx.body = {
     ok: true,
-    foundUser
+    data,
+    jwt
   }
 }
 
