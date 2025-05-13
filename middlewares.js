9baa0206d2e2e3f17486d53bb8784d25bcecd2d0
sebandresve <sebandres.ve@gmail.com>
2025-05-13 18:17:46 -0400

    se protejen rutas con validacion de jwt

--- middlewares.js
+++ middlewares.js
@@ -1,3 +1,5 @@
+import { verifyToken } from './src/utils/tokenGenerator.js'
+
 export async function setFinalResponseMdw (ctx, next) {
   await next()
   const rt = ctx.response.get('X-Response-Time')
@@ -50,4 +52,31 @@ export function bodyParserMdw () {
     }
     await next()
   }
+}
+
+export const validateTokenMiddleware = async (ctx, next) => {
+  const BEARER_START = 'Bearer '
+  const checkStringStartWith = (str, start) => str.startsWith(start)
+
+  function checkTokenExists (token) {
+    if (!token) {
+      throw new Error('Token not found')
+    }
+
+    if (!checkStringStartWith(token, BEARER_START)) {
+      throw new Error('Token invalid format')
+    }
+
+    const bearerJwt = token.split(BEARER_START)[1]
+    return bearerJwt
+  }
+
+  const token = checkTokenExists(ctx.headers.authorization)
+
+  try {
+    ctx.currentUser = await verifyToken(token)
+  } catch (error) {
+    throw new Error('Error parsing token')
+  }
+  await next()
 }
--- src/userRouter.js
+++ src/userRouter.js
@@ -1,15 +1,16 @@
 import Router from 'koa-router'
 import { userCtr } from './userCtr.js'
 import { loginCtr } from './loginCtr.js'
+import { validateTokenMiddleware } from '../middlewares.js'
 
 const router = new Router()
 
 router.post('/login', loginCtr.signIn)
 
-router.get('/user/:id', userCtr.getUserById)
-router.get('/user', userCtr.getAllUsers)
+router.get('/user/:id', validateTokenMiddleware, userCtr.getUserById)
+router.get('/user', validateTokenMiddleware, userCtr.getAllUsers)
 router.post('/user', userCtr.createUser)
-router.put('/user/:id', userCtr.updateUser)
-router.delete('/user/:id', userCtr.deleteUser)
+router.put('/user/:id', validateTokenMiddleware, userCtr.updateUser)
+router.delete('/user/:id', validateTokenMiddleware, userCtr.deleteUser)
 
 export default router
